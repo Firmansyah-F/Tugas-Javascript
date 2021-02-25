98bbc16ae85ed98ec5f08c43f7e0f9a9b3c44b0f
Fikri <fikrifirmansyah096@gmail.com>
2021-02-25 08:30:27 +0700

    update

--- CensoringWord.js
+++ CensoringWord.js
@@ -1,13 +1,6 @@
-// Censors these words: dolor, elit, quis, nisi, fugiat, proident, laborum; of this paragraph.
+let Words = ["dolor", "elit", "quis", "nisi", "fugiat", "proident", "laborum"];
+let str = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-var s = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-var words = ['dolor','elit','quis', 'nisi', 'fugiat', 'proident', 'laborum']
-var censore =['*****','****','****','****','******','********','*******']
-var array = s.split(' ')
-console.log(array)
+str = str.replace(new RegExp(Words.join('|'), 'g'), '*****');
 
-for (let i = 0; i<array.length; i++){
-    if (array[i] == words) {
-        array[i] == censore
-    }
-}
+console.log(str)
