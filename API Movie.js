20b80f1a48b1e4ec740c9683a45b931ee86e5d4a
Fikri <fikrifirmansyah096@gmail.com>
2021-03-01 23:21:47 +0700

    update

--- API Movie.js
+++ API Movie.js
@@ -1,19 +1,59 @@
 const axios = require("axios").default;
 
-const Movie = async () => {
-    try {
-        const result = await axios.get(
-            "https://api.themoviedb.org/3/movie/550?api_key=bff9192b3201ae7c1be28a79cc309510",
-            // {
-            //     headers: {
-            //         key: `e634e5410ecb00e93779b3be44547d24`,
-            //     },
-            // }
-        );
+// Soal No 3 Point no 4
+let numb = 800
+// for (let i=0; i<100; i++){
 
-        console.log(result.data);
-    } catch (error) {
-        console.log({ error });
-    }
-};
-Movie()
+//     const Movie = async () => {
+//         try {
+//             const result = await axios.get(
+//                 `https://api.themoviedb.org/3/movie/${numb}?api_key=bff9192b3201ae7c1be28a79cc309510`);
+//                 let average = result.data.vote_average
+//                 let date = result.data.release_date
+//                 let title = result.data.title
+//                 let objDate = new Date(date);
+//                 var year = objDate.getFullYear()
+//                 if (average > 7.5 && year === 2016 ){
+//                     console.log(`Vote Average > 7.5 : ${average} => Title : ${title} `);
+//                     console.log(`Release year on 2016 : ${year}`)
+//                 }else {
+//                     console.log(`Vote Average :  ${average} => Tile : ${title}`);
+//                     console.log(`Release year : ${year}`)
+//                 }
+               
+//         } catch (error) {
+//             console.log({ error });
+//         }
+//     };
+//     Movie()
+//     numb += 1 
+// }
+
+// Point 1
+for (let i=0; i<100; i++){
+
+    const Movie = async () => {
+        try {
+            const result = await axios.get(
+                `https://api.themoviedb.org/3/movie/${numb}?api_key=bff9192b3201ae7c1be28a79cc309510`);
+                let language = result.data.original_language
+                let title = result.data.title
+
+                if (language === 'id' ){
+                    console.log(`Original_Languange Indonesia : ${language} => Title : ${title} `);
+                }else {
+                    console.log(`Original_Languange : ${language} => Title : ${title} `);
+                }
+               
+        } catch (error) {
+            console.log({ error });
+        }
+    };
+    Movie()
+    numb += 1 
+}
+
+
+
+
+
--- Screping.js
+++ Screping.js
@@ -1,24 +1,40 @@
 
-const puppeteer = require('puppeteer');
-const $ = require('cheerio');
+// const puppeteer = require('puppeteer');
+// const $ = require('cheerio');
 
-const url = 'https://www.kompas.com/';
+// const url = 'https://www.kompas.com/';
     
-    puppeteer
-      .launch()
-      .then(function(browser) {
-        return browser.newPage();
-      })
-      .then(function(page) {
-        return page.goto(url).then(function() {
-          return page.content();
-        });
-      })
-      .then(function(html) {
-        $('h2', html).each(function() {
-          console.log($(this).text());
-        });
-      })
-      .catch(function(err) {
-        //handle error
+//     puppeteer
+//       .launch()
+//       .then(function(browser) {
+//         return browser.newPage();
+//       })
+//       .then(function(page) {
+//         return page.goto(url).then(function() {
+//           return page.content();
+//         });
+//       })
+//       .then(function(html) {
+//         $('h2', html).each(function() {
+//           console.log($(this).text());
+//         });
+//       })
+//       .catch(function(err) {
+//         //handle error
+//       });
+
+
+      const request = require("request");
+      const cheerio = require("cheerio");
+      
+      request("https://www.kompas.com/", (err, res, html) => {
+        if (!err && res.statusCode == 200) {
+          const $ = cheerio.load(html);
+      
+          $(".most__list.clearfix").each((i, el) => {
+            const title = $(el).find(".most__title").text();
+            const link = $(el).find("a").attr("href");
+            console.log(title, link);
+          });
+        }
       });
