b12edf8752da880b11aa8d0c4387bd4d97e130f2
Fikri Firmansyah <60101886+Firmansyah-F@users.noreply.github.com>
2021-03-16 08:06:06 +0700

    Delete RestApiBook directory

--- RestApiBook/app/utils/middleware/errorHandling.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-    const payloadError = {
-        success: false,
-        message: error.message,
-        data: error,
-    };
-    return res.json(payloadError);
-}
-module.exports = { errorHandler }
