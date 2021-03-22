4941e3734edd45c2a8f7207df95665557ac537e2
Fikri Firmansyah <60101886+Firmansyah-F@users.noreply.github.com>
2021-03-22 09:30:18 +0700

    Delete RestApiBook directory

--- RestApiBook/app/utils/middleware/morgan.js
+++
@@ -1,8 +0,0 @@
-const morgan = require ('morgan')
-const fs = require ('fs')
-const path = require('path')
-const loggerStream = fs.createWriteStream(path.join(__dirname,"./../../../temp/logger.log"),{flags:"a"})
-
-const logger = morgan("tiny", {stream:loggerStream})
-
-module.exports = { logger }
